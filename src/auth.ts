b326b9bc4f270c7675ea12506a6271318398c7f9
Rafael Singer <rafaelsinger66@gmail.com>
2025-04-13 19:38:14 -0400

    add prisma adapter

--- src/auth.ts
+++ src/auth.ts
@@ -2,15 +2,35 @@
 // @ts-nocheck
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { User } from './types/user';
+import { prisma } from './lib/prisma';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  adapter: PrismaAdapter(prisma),
   callbacks: {
-    async signIn({ account, profile }) {
-      if (account.provider === 'google') {
-        return profile.email_verified && profile.email.endsWith('@bc.edu');
+    signIn({ account, profile }) {
+      if (
+        account.provider === 'google' &&
+        profile.email_verified &&
+        profile.email.endsWith('@bc.edu')
+      ) {
+        return true;
       }
       return false;
+    },
+    jwt({ token, user }) {
+      console.log({ token, user });
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session({ session, user }) {
+      console.log({ session, user });
+      session.user.id = user.id;
+      return session;
     },
   },
 });
