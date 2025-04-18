3a1b50dc005438b1fc2633f36560611b1bed82db
Rafael Singer <rafaelsinger66@gmail.com>
2025-04-17 23:07:55 -0500

    Squashed commit of the following:
    commit 78b3bbe8f79c3f9a881b833baf5d84e0986a059e
    Author: Rafael Singer <rafaelsinger66@gmail.com>
    Date:   Thu Apr 17 14:48:57 2025 -0500
    
        fix build script
    
    commit d7f2d75f6a05183d116e4ccfebda1b5afde210bd
    Author: Rafael Singer <rafaelsinger66@gmail.com>
    Date:   Thu Apr 17 14:46:05 2025 -0500
    
        add new build script

---
+++ ignore-build.js
@@ -0,0 +1,11 @@
+const branch = process.env.VERCEL_GIT_COMMIT_REF;
+const environment = process.env.VERCEL_ENV; // "production", "preview", "development"
+
+// skip if not production or not staging
+if (environment !== 'production' || (environment === 'preview' && branch !== 'staging')) {
+  console.log(`🛑 Ignoring build: branch is "${branch}" and not "staging"`);
+  process.exit(0); // skip the build
+}
+
+console.log(`✅ Proceeding with build on branch "${branch}" in "${environment}" environment`);
+process.exit(1); // continue with build
