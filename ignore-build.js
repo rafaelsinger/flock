ea0ec8e13b6191a9e9ab349c1055088d1d65dbb8
Rafael Singer <rafaelsinger66@gmail.com>
2025-04-17 23:20:44 -0500

    fix build script fr fr

--- ignore-build.js
+++ ignore-build.js
@@ -1,11 +1,11 @@
 const branch = process.env.VERCEL_GIT_COMMIT_REF;
-const environment = process.env.VERCEL_ENV; // "production", "preview", "development"
+const environment = process.env.VERCEL_ENV;
 
-// skip if not production and not staging
-if (environment !== 'production' && environment === 'preview' && branch !== 'staging') {
-  console.log(`🛑 Ignoring build: branch is "${branch}" and not "staging"`);
-  process.exit(0); // skip the build
+// Only build if it's production OR if it's preview and the branch is staging
+if (environment === 'production' || (environment === 'preview' && branch === 'staging')) {
+  console.log(`✅ Proceeding with build on branch "${branch}" in "${environment}" environment`);
+  process.exit(1); // continue with build
 }
 
-console.log(`✅ Proceeding with build on branch "${branch}" in "${environment}" environment`);
-process.exit(1); // continue with build
+console.log(`🛑 Ignoring build: branch is "${branch}" in "${environment}" environment`);
+process.exit(0); // skip the build
