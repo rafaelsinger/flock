4c35e81cd0071614324ccdeb85d15c9af1c8e689
Rafael Singer <rafaelsinger66@gmail.com>
2025-04-20 18:59:21 -0400

    make migration idempotent

--- prisma/migrations/20250420215404_add_internship_postgrad_type/migration.sql
+++ prisma/migrations/20250420215404_add_internship_postgrad_type/migration.sql
@@ -1,2 +1,14 @@
--- AlterEnum
-ALTER TYPE "PostGradType" ADD VALUE 'internship';
+DO $$ 
+BEGIN 
+    -- Check if the enum value doesn't already exist
+    IF NOT EXISTS (
+        SELECT * 
+        FROM pg_enum
+        JOIN pg_type ON pg_type.oid = pg_enum.enumtypid
+        WHERE pg_type.typname = 'PostGradType'
+        AND pg_enum.enumlabel = 'internship'
+    ) THEN
+        -- Only add the value if it doesn't exist
+        ALTER TYPE "PostGradType" ADD VALUE 'internship';
+    END IF;
+END $$;
