16763567f09d065c261dfa788dee96af6ff0353b
Rafael Singer <rafaelsinger66@gmail.com>
2025-04-20 23:14:31 -0400

    revert internship postgradtype migration since prisma gets mad

--- prisma/migrations/20250420215404_add_internship_postgrad_type/migration.sql
+++ prisma/migrations/20250420215404_add_internship_postgrad_type/migration.sql
@@ -1,14 +1,2 @@
-DO $$ 
-BEGIN 
-    -- Check if the enum value doesn't already exist
-    IF NOT EXISTS (
-        SELECT * 
-        FROM pg_enum
-        JOIN pg_type ON pg_type.oid = pg_enum.enumtypid
-        WHERE pg_type.typname = 'PostGradType'
-        AND pg_enum.enumlabel = 'internship'
-    ) THEN
-        -- Only add the value if it doesn't exist
-        ALTER TYPE "PostGradType" ADD VALUE 'internship';
-    END IF;
-END $$;
+-- AlterEnum
+ALTER TYPE "PostGradType" ADD VALUE 'internship';
